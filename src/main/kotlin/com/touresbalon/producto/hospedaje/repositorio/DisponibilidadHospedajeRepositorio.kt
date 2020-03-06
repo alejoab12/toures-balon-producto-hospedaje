0d796501c173108a75bedfeed763063a031c3757
malcalab <malcalab@everis.com>
2020-03-06 09:50:36 -0500

    Se elimina dependencias de cassandra

--- src/main/kotlin/com/touresbalon/producto/hospedaje/repositorio/DisponibilidadHospedajeRepositorio.kt
+++ src/main/kotlin/com/touresbalon/producto/hospedaje/repositorio/DisponibilidadHospedajeRepositorio.kt
@@ -1,10 +1,8 @@
 package com.touresbalon.producto.hospedaje.repositorio
 
-import com.datastax.driver.core.LocalDate
+
 import com.touresbalon.producto.hospedaje.entidad.DisponibilidadHospedaje
 import org.bson.types.ObjectId
-import org.springframework.data.cassandra.repository.CassandraRepository
-import org.springframework.data.cassandra.repository.Query
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 import java.util.*
